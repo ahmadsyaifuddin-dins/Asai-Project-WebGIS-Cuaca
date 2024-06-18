969d0b89844dbbf425bd6e754d75cc4c072446d7
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-99@users.noreply.github.com>
2024-06-19 00:20:44 +0800

    Update position.js

--- assets/js/position.js
+++ assets/js/position.js
@@ -21,10 +21,10 @@ function showPosition(position) {
     // seperti menambahkan marker ke peta atau mengirimkan ke server.
     // Berikut ini contoh menambahkan marker ke peta:
     L.marker([latitude, longitude]).addTo(map)
-        .bindPopup('Your Location').openPopup();
+        .bindPopup('Posisi kamu disini nih (⁠≧⁠▽⁠≦⁠)').openPopup();
 
     // Anda juga bisa memperbarui tampilan peta agar menampilkan posisi pengguna:
-    map.setView([latitude, longitude], 13);
+    map.setView([latitude, longitude], 15);
 }
 
 // Panggil fungsi getLocation() saat halaman dimuat.
